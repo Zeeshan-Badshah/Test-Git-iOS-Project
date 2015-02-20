7db9ccb4907b88c759637e08f92e4a33ed3203aa
Ali Naveed <Ali@Alis-Mac-mini.local>
2015-02-20 22:00:14 +0500

    added new test file

---
+++ Git Practic/NewTestClass.h
@@ -0,0 +1,13 @@
+//
+//  NewTestClass.h
+//  Git Practic
+//
+//  Created by Ali Naveed on 2/20/15.
+//  Copyright (c) 2015 ITbees. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewTestClass : NSObject
+
+@end
---
+++ Git Practic/NewTestClass.m
@@ -0,0 +1,13 @@
+//
+//  NewTestClass.m
+//  Git Practic
+//
+//  Created by Ali Naveed on 2/20/15.
+//  Copyright (c) 2015 ITbees. All rights reserved.
+//
+
+#import "NewTestClass.h"
+
+@implementation NewTestClass
+
+@end
