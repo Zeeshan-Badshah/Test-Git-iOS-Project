15c121128c2ab5af43d83856b2c7dcbe63c790a8
Ali Naveed <Ali@Alis-Mac-mini.local>
2015-02-20 21:22:35 +0500

    Initial Commit

---
+++ Git Practic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Practic
+//
+//  Created by Ali Naveed on 2/20/15.
+//  Copyright (c) 2015 ITbees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
